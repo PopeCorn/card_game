e545e0cfdb40d37ffc0e103cdc256dcc0b6bfb9a
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-19 15:30:18 +0100

    Added Tom healing

--- test1.py
+++ test1.py
@@ -13,7 +13,9 @@ class Tom:
         dice = random.randrange(0, 6)
         blow = Tom.attack + dice
         oponent.hp -= blow
-    
+    def tom_heal():
+        restore = random.randrange(0, 4)
+        Tom.hp += restore
 
 
         
