bfbdce5b8191d62dd22c6fb3f902332673dc82da
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-19 14:26:32 +0100

    Tom_file
    A file for a character named Tom

---
+++ test1.py
@@ -0,0 +1,24 @@
+import random
+
+
+class Tom:
+    def __init__(self):
+        self.hp = 20
+        self.attack = 4
+        self.defence = 2
+    def tom_attack(oponent):
+        blow = Tom.attack - oponent.defence
+        oponent.hp -= blow
+    def tom_special(oponent):
+        dice = random.randrange(0, 4)
+        blow = Tom.attack + dice
+        oponent.hp -= blow
+    
+
+
+        
+
+
+        
+
+
