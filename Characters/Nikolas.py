4fbe9a8930378d5a20f386741443259867fd0545
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-20 21:56:10 +0100

    Update Nikolas.py
    Nikolas -> self

--- Characters/Nikolas.py
+++ Characters/Nikolas.py
@@ -7,13 +7,13 @@ class Nikolas:
         self.defence = 1
         self.regeneration = 2
     
-    def nikolas_attack(oponent):
-        Nikolas.energy -= 3
+    def nikolas_attack(self, oponent):
+        self.energy -= 3
         blow = 4 - oponent.defence
         oponent.hp -= blow
     
-    def nikolas_special(oponent):
-        Nikolas.energy -= 6
+    def nikolas_special(self, oponent):
+        self.energy -= 6
         roll = random.randrange(1, 10)
         blow = roll - oponent.defence
         oponent.hp -= blow
