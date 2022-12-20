293d72de473d323093a3c91ac8d5cdaf0e665b31
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-20 21:57:39 +0100

    Update Tom.py
    Tom -> self

--- Characters/Tom.py
+++ Characters/Tom.py
@@ -5,15 +5,16 @@ class Tom:
         self.defence = 1
         self.regeneration = 2
     
-    def tom_attack(oponent):
-        Tom.energy -= 4
+    def tom_attack(self, oponent):
+        self.energy -= 4
         blow = 4 - oponent.defence
         oponent.hp -= blow
     
-    def tom_special(oponent):
-        Tom.energy -= 6
+    def tom_special(self, oponent):
+        self.energy -= 6
         blow = 5 - oponent.defence
         oponent.hp -= blow
+        
     def tom_energy_regeneration(self):
         if self.energy == 7:
             pass
