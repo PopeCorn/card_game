81c9defc7de27e9bd97d79f4b06d1617cc8dd7f0
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-23 10:45:19 +0100

    Update Žimík.py
    Fixed zimik attacking

--- Characters/Žimík.py
+++ Characters/Žimík.py
@@ -11,9 +11,9 @@ class Zimik:
     def zimik_attack(self, oponent):
         self.energy -= 4
         blow = 3 - oponent.defence
-        f.attacking(oponent, blow)
+        f.attacking(oponent, blow, 3)
 
     def zimik_special(self, oponent):
         self.energy -= 6
         blow = 5 - oponent.defence
-        f.attacking(oponent, blow)
+        f.attacking(oponent, blow, 5)
