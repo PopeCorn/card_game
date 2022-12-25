67ee0a4b5e84f2bfcb7b5b87087c06ca1590d933
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-25 12:47:28 +0100

    Update main.py
    Imported settings

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from Characters import David, Honimír, Honza, Kvítek, Mark, Máta, Milan, Nikolas, Pavel, Petr, Tom, Žimík
+from Code import settings as s
 
 # Nic zatím nedělat
 
 # Toto je jen příklad
 if __name__ == "__main__":
-    davídek = David.David()
-    print(davídek.hp)
+    while True:
+        s.count += 1
+        davídek = David.David()
+        print(davídek.hp)
+        exit()
+        
+        
