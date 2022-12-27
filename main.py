fbf55feaa2e9dbd3169c3304796b8b72b35b0fc9
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-27 14:22:45 +0100

    I messed up"

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
 from Characters import David, Mojmir, Honza, Kvítek, Mark, Máta, Milan, Nikolas, Pavel, Petr, Tom, Žimík
 from Code import settings as s
+from colorama import Fore
 
-# Nic zatím nedělat
+available_characters = ['david', 'matyas', 'mojmir', 'honza', 'zimik', 'kvitek', 'mark', 'milan', 'nikolas', 'pavel', 'petr', 'tom']
+first_player_collection = []
+second_player_collection = []
+
+def choose_character(player_list, number):
+    for i in range(1, 4):
+        while True:
+                new_character = input(f'PLAYER {number}, select your new character (You will have 3 of them in total): ').lower()
+                if (new_character in available_characters):
+                    if (new_character in player_list):
+                        print(f'{Fore.RED}You already have that character!{Fore.RESET}')
+                        continue
+                    else:
+                        print(f'{Fore.BLUE}{new_character.capitalize()} added! {Fore.RESET}')
+                        player_list.append(new_character)
+                        break
+                else:
+                    print(f'{Fore.RED}That character is not available!{Fore.RESET}')
+                    continue
 
 s.count = 0
 if __name__ == "__main__":
-    david = David.David()
-    mata = Máta.Matyas()
+    print('''Available characters:
+        David
+        Matyas
+        Mojmir
+        Honza
+        Zimikcl
+        Kvitek
+        Mark
+        Milan
+        Nikolas
+        Pavel
+        Petr
+        Tom''')
+    choose_character(first_player_collection, 1)
+    print('---------------------------------------')
+    choose_character(second_player_collection, 2)
+    print(f'First player: {first_player_collection}')
+    print(f'Second player: {second_player_collection}')
+    exit()
     while True:
         s.count += 1
         if s.mata_poison is True:
             mata.poison(s.mata_poison_target)
             s.mata_poison = False
-        exit()
         
         
