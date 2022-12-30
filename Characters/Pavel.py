b20d863861faa19ed5307de7c829f587ffc4b4de
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-30 13:46:17 +0100

    Pavel, Petr, Tom, Zimik done

--- Characters/Pavel.py
+++ Characters/Pavel.py
@@ -7,14 +7,23 @@ class Pavel:
         self.energy = 8
         self.max_energy = 8
         self.defence = 8
+        self.cooldown = 0
+        self.special_cooldown = 0
 
     def attack(self, oponent):
         f.attack(self.energy, 3, 3, oponent)
 
     def special(self, oponent):
-        f.attack(self.energy, 7, 8, oponent)
+        f.attack(self.energy, 7, 8, oponent, 2, self.cooldown, special=True)
 
     def multiple_attacks(self, oponent):
-        self.energy -= 7
-        f.attack(self.energy, 0, 3, oponent)
-        f.attack(self.energy, 0, 3, oponent)
+        if self.special_cooldown > 0:
+            print(f'You can use this ability in {self.special_cooldown} rounds!')
+        else:
+            if self.energy < 7:
+                print('You do not have enough energy!')
+            else:
+                self.special_cooldown += 2
+                self.energy -= 7
+                f.attack(self.energy, 0, 3, oponent)
+                f.attack(self.energy, 0, 3, oponent)
