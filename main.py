5a60f4471a0ea062aa0f4cc4d74001cfe8e286d9
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2022-12-31 17:32:54 +0100

    Round for 1st player done for now, yet to discover bugs

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from Code import settings as s
 from Code import functions as f
 from colorama import Fore
 
+def special_attacks(main_character):
+    while True:
+        oponent = input('Who do you want to attack: ')
+        if oponent in all_unplayable:
+            main_character.special(transfer[oponent])
+            break
+        else:
+            print('That character is not in the game!')
+            continue
+
 
 def poison_checking():
     if s.mata_poison is True:
@@ -10,46 +20,6 @@ def poison_checking():
         s.mata_poison = False
     else:
         pass
-
-def using(collection, new_collection):
-        for unused in collection:
-            if unused == 'david':
-                david = David.David()
-                new_collection.append(david)
-            elif unused == 'honza':
-                honza = Honza.Honza()
-                new_collection.append(honza)
-            elif unused == 'kvitek':
-                kvitek = Kvítek.Kvitek()
-                new_collection.append(kvitek)
-            elif unused == 'mark':
-                mark = Mark.Marekec()
-                new_collection.append(mark)
-            elif unused == 'matyas':
-                s.mata_here = True
-                matyas = Matyas.Matyas()
-                new_collection.append(matyas)
-            elif unused == 'milan':
-                milan = Milan.Milan()
-                new_collection.append(milan)
-            elif unused == 'mojmir':
-                mojmir = Mojmir.Mojmir()
-                new_collection.append(mojmir)
-            elif unused == 'nikolas':
-                nikolas = Nikolas.Nikolas()
-                new_collection.append(nikolas)
-            elif unused == 'pavel':
-                pavel = Pavel.Pavel()
-                new_collection.append(pavel)
-            elif unused == 'petr':
-                petr = Petr.Petr()
-                new_collection.append(petr)
-            elif unused == 'tom':
-                tom = Tom.Tom()
-                new_collection.append(tom)
-            elif unused == 'zimik':
-                zimik = Žimík.Zimik()
-                new_collection.append(zimik)
 
 
 if __name__ == "__main__":
@@ -72,19 +42,65 @@ if __name__ == "__main__":
     f.choose_character(first_player_collection, 1, available_characters)
     print('---------------------------------------')
     f.choose_character(second_player_collection, 2, available_characters)
-    all_unplayable = first_player_collection + second_player_collection
+    all_unplayable = (first_player_collection + second_player_collection)
     transfer = {}
-    first_player_playable = []
-    second_player_playable = []
-    all_playable = first_player_playable + second_player_playable
-    for i in range(0, 7):
-        transfer[all_unplayable[i]] = all_playable[i]
-    inverted_transfer = {v: k for k, v in transfer.items()}
-    using(first_player_collection, first_player_playable)
-    using(second_player_collection, second_player_playable)
+    all_playable = []
+    for unused in all_unplayable:
+            if unused == 'david':
+                david = David.David()
+                transfer['david'] = david
+                all_playable.append(david)
+            elif unused == 'honza':
+                honza = Honza.Honza()
+                transfer['honza'] = honza
+                all_playable.append(honza)
+            elif unused == 'kvitek':
+                kvitek = Kvítek.Kvitek()
+                transfer['kvitek'] = kvitek
+                all_playable.append(kvitek)
+            elif unused == 'mark':
+                mark = Mark.Marekec()
+                transfer['mark'] = mark
+                all_playable.append(mark)
+            elif unused == 'matyas':
+                s.mata_here = True
+                matyas = Matyas.Matyas()
+                transfer['matyas'] = matyas
+                all_playable.append(matyas)
+            elif unused == 'milan':
+                milan = Milan.Milan()
+                transfer['milan'] = milan
+                all_playable.append(milan)
+            elif unused == 'mojmir':
+                mojmir = Mojmir.Mojmir()
+                transfer['mojmir'] = mojmir
+                all_playable.append(mojmir)
+            elif unused == 'nikolas':
+                nikolas = Nikolas.Nikolas()
+                transfer['nikolas'] = nikolas
+                all_playable.append(nikolas)
+            elif unused == 'pavel':
+                pavel = Pavel.Pavel()
+                transfer['pavel'] = pavel
+                all_playable.append(pavel)
+            elif unused == 'petr':
+                petr = Petr.Petr()
+                transfer['petr'] = petr
+                all_playable.append(petr)
+            elif unused == 'tom':
+                tom = Tom.Tom()
+                transfer['tom'] = tom
+                all_playable.append(tom)
+            elif unused == 'zimik':
+                zimik = Žimík.Zimik()
+                transfer['zimik'] = zimik
+                all_playable.append(zimik)
 
+    inverted_transfer = {v: k for k, v in transfer.items()}
     action_finish = {}
     f.initialize_dict(action_finish, all_playable)
+
+
     index = 0
     while True:
         s.count += 1
@@ -92,7 +108,7 @@ if __name__ == "__main__":
         f.regeneration(all_playable)
         if s.mata_here:
             poison_checking()
-        for character in first_player_playable:
+        for character in all_playable[0, 4]:
             print(f'''Player 1, choose your action with {str(first_player_collection[index]).capitalize} (TYPE ITS NUMBER):
             1. Attack
             2. Special attack
@@ -109,90 +125,35 @@ if __name__ == "__main__":
                         continue
 
                 elif action == '3':
-                    if inverted_transfer[character] == 'david':
+                    if inverted_transfer[character] == 'david' or inverted_transfer[character] == 'honza' or inverted_transfer[character] == 'mark' or inverted_transfer[character] == 'nikolas' or inverted_transfer[character] == 'mojmir':
                         character.special()
 
-                    elif inverted_transfer[character] == 'honza':
-                        character.special()
-
-                    elif inverted_transfer[character] == 'kvitek':
-                        while True:
-                            oponent = input('Who do you want to attack: ')
-                            if oponent in all_unplayable:
-                                character.special(transfer[oponent])
-                                break
-                            else:
-                                print('That character is not in the game!')
-                                continue
-
-                    elif inverted_transfer[character] == 'mark':
-                        character.special()
-                    elif inverted_transfer[character] == 'matyas':
-                        while True:
-                            oponent = input('Who do you want to attack: ')
-                            if oponent in all_unplayable:
-                                character.special(transfer[oponent])
-                                break
-                            else:
-                                print('That character is not in the game!')
-                                continue
-
-                    elif inverted_transfer[character] == 'milan':
-                        while True:
-                            oponent = input('Who do you want to attack: ')
-                            if oponent in all_unplayable:
-                                character.special(transfer[oponent])
-                                break
-                            else:
-                                print('That character is not in the game!')
-                                continue
-
-                    elif inverted_transfer[character] == 'mojmir':
-                        character.special()
-
-                    elif inverted_transfer[character] == 'nikolas':
-                        character.special()
-
-                    elif inverted_transfer[character] == 'pavel':
-                        while True:
-                            oponent = input('Who do you want to attack: ')
-                            if oponent in all_unplayable:
-                                character.special(transfer[oponent])
-                                break
-                            else:
-                                print('That character is not in the game!')
-                                continue
-
-                    elif inverted_transfer[character] == 'petr':
-                        while True:
-                            oponent = input('Who do you want to attack: ')
-                            if oponent in all_unplayable:
-                                character.special(transfer[oponent])
-                                break
-                            else:
-                                print('That character is not in the game!')
-                                continue
+                    elif inverted_transfer[character] == 'kvitek' or inverted_transfer[character] == 'matyas' or inverted_transfer[character] == 'milan' or inverted_transfer[character] == 'pavel' or inverted_transfer[character] == 'petr' or inverted_transfer[character] == 'zimik':
+                        special_attacks(character)
 
                     elif inverted_transfer[character] == 'tom':
-                        healing = input('Do you want to heal yourself [1] or someone other[2] (Type the number): ')
-                        if healing == '1':
-                            character.special()
-                        elif healing == '2':
-                            member = input('Who do you want to heal: ')
-                            if member in all_unplayable:
-                                character.special(transfer[member], not_self=True)
-
-                    elif inverted_transfer[character] == 'zimik':
-                         while True:
-                            oponent = input('Who do you want to attack: ')
-                            if oponent in all_unplayable:
-                                character.special(transfer[oponent])
+                        while True:
+                            healing = input('Do you want to heal yourself [1] or someone other[2] (Type the number): ')
+                            if healing == '1':
+                                character.special()
                                 break
+                            elif healing == '2':
+                                while True:
+                                    member = input('Who do you want to heal: ')
+                                    if member in first_player_collection:
+                                        character.special(transfer[member], not_self=True)
+                                    else:
+                                        print('That player either does not exist or is not on your team!')
+                                        continue
+                                    break
                             else:
-                                print('That character is not in the game!')
+                                print('That is not an option!')
                                 continue
-
-        # KAŽDÉ KOLO BUDOU OBA HRÁČI HRÁT SE VŠEMI SVÝMI CHARAKTERY POMOCÍ FOR LOOPU
+                            break
+                break
+        index = 0
+        for character in all_playable[4, 7]:
+            pass
         exit()
         
         
