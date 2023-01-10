5005127c89475f258dddb953be4c81b534b3d026
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2023-01-10 14:24:29 +0100

    Main.py changes

---
+++ card_game_v2/main.py
@@ -0,0 +1,18 @@
+from Characters import David, Matyas, Mojmir, Honza, Kvítek, Mark, Milan, Nikolas, Pavel, Petr, Tom, Žimík
+from Code import settings as s
+from Code import functions as f
+
+if __name__ == "__main__":
+    print('''Available characters:
+        David
+        Matyas
+        Mojmir
+        Honza
+        Zimik
+        Kvitek
+        Mark
+        Milan
+        Nikolas
+        Pavel
+        Petr
+        Tom''')
