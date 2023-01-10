fea66721174f6f8105a24160c77784ab26574658
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2023-01-10 15:13:53 +0100

    cstkinter

--- card_game_v2/main.py
+++ card_game_v2/main.py
@@ -1,9 +1,31 @@
-from Characters import David, Matyas, Mojmir, Honza, Kvítek, Mark, Milan, Nikolas, Pavel, Petr, Tom, Žimík
-from Code import settings as s
-from Code import functions as f
+import customtkinter as cstki
 
-if __name__ == "__main__":
-    print('''Available characters:
+cstki.set_appearance_mode('dark')
+cstki.set_default_color_theme('green')
+
+first_player = []
+second_player = []
+def adding(player):
+    player.append(entry)
+
+
+root = cstki.CTk()
+root.geometry('500x300')
+
+frame = cstki.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill='both', expand=True)
+
+label = cstki.CTkLabel(master=frame, text='Card game')
+label.pack(pady=12, padx=10)
+
+entry = cstki.CTkEntry(master=frame, placeholder_text='New character')
+entry.pack(pady=12, padx=10)
+
+button = cstki.CTkButton(master=frame, text='Add', command=adding(first_player))
+button.pack(pady=12, padx=10)
+
+textbox = cstki.CTkTextbox(master=root)
+textbox.insert("0.0", '''Available characters:
         David
         Matyas
         Mojmir
@@ -16,3 +38,9 @@ if __name__ == "__main__":
         Pavel
         Petr
         Tom''')
+text = textbox.get("0.0", "end")
+textbox.pack(pady=20, padx=20)
+
+if __name__ == '__main__':
+    root.mainloop()
+    print(first_player)
