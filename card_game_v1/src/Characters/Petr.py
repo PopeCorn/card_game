dd65be5207c8a33ce6b1f8d497a50254c318bc3e
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2023-01-28 17:58:41 +0100

    Update Petr.py
    v1 rework

--- card_game_v1/src/Characters/Petr.py
+++ card_game_v1/src/Characters/Petr.py
@@ -8,12 +8,12 @@ class Petr:
         self.cooldown, self.special_cooldown = 0, 0
 
     def attack(self, oponent):
-        f.attack(self.energy, 3, 3, oponent)
+        self.energy = f.attack(self.energy, 3, 3, oponent)
 
     def special_attack(self, oponent):
-        f.attack(self.energy, 6, 5, oponent, 2, self.cooldown, special=True)
+        self.energy, self.cooldown = f.attack(self.energy, 6, 5, oponent, 2, self.cooldown, special=True)
 
     # Petr uses his unmatched speed give himself energy and attack at the same time
     def special(self, oponent):
         self.energy += 3
-        f.attack(self.energy, 4, 2, oponent, 1, self.special_cooldown, special=True)
+        self.energy = f.attack(self.energy, 4, 2, oponent, 1, self.special_cooldown, special=True)
